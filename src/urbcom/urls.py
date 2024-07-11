fc1d2d1128a3321fb1da6fe5097510e434af3524
lucydunshea <125615453+lucydunshea@users.noreply.github.com>
2024-07-12 11:58:57 +1200

    added readme

--- src/urbcom/urls.py
+++ src/urbcom/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from views import home, about_us, contact
+from src.views import home, about_us, contact
 from projects import views
 
 urlpatterns = [

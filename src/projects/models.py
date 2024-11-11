6d4820ea7e6b08fa3fdce335f033ad8494529c63
lucydunshea <125615453+lucydunshea@users.noreply.github.com>
2024-11-11 20:23:05 +0100

    added project migrations

--- src/projects/models.py
+++ src/projects/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-
+from blog.models import Category
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     image = models.ImageField(upload_to='projects/')
     url = models.URLField(blank=True)
+    categories = models.ManyToManyField(Category, blank=True)
 
     def __str__(self):
         return self.title
